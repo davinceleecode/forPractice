7a6ba40ee6032ab7ee0334fac87c8d65bfaab2bb
davinceleecode <vincent.lee.c.flores@gmail.com>
2025-03-17 15:11:24 +0800

    Added a new project

---
+++ DesignPatterns/Singleton/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Singleton
+{
+    public class Class1
+    {
+
+        public Class1()
+        {
+
+        }
+    }
+}
