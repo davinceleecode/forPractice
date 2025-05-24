587bd69d6cc23e581c6b1496241915e45a5a62ab
davinceleecode <wiz22vincent@gmail.com>
2025-05-24 23:53:28 +0800

    factory method

---
+++ DesignPatterns/Behavioral Patterns/Strategy/Strategy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace forPractice.DesignPatterns.Behavioral_Patterns.Strategy
+{
+    public class Strategy
+    {
+        interface IMessage
+        {
+            void Send();
+        }
+
+
+        class SMS : IMessage
+        {
+            public void Send()
+            {
+                Console.WriteLine("Sending SMS...");
+            }
+        }
+
+        class Email : IMessage
+        {
+            public void Send()
+            {
+                Console.WriteLine("Sending SMS...");
+            }
+        }
+
+
+
+        class Notify
+        {
+            public void SendMessage(IMessage message)
+            {
+                message.Send();
+            }
+
+        }
+
+
+
+        class Implementation
+        {
+            public void SendMessage()
+            {
+                IMessage SMS = new SMS();
+                IMessage Email = new Email();
+
+                Notify notify = new Notify();
+                notify.SendMessage(SMS);
+                notify.SendMessage(Email);
+            }
+        }
+    }
+}
---
+++ DesignPatterns/CreationalPatterns/FactoryMethod/FactoryMethod.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace forPractice.DesignPatterns.CreationalPatterns.FactoryMethod
+{
+    public class FactoryMethod
+    {
+
+        interface IMessage
+        {
+            void Send();
+        }
+
+
+        class SMS : IMessage
+        {
+            public void Send()
+            {
+                Console.WriteLine("Sending SMS...");
+            }
+        }
+
+        class Email : IMessage
+        {
+            public void Send()
+            {
+                Console.WriteLine("Sending Email...");
+            }
+        }
+
+
+
+
+        class Notify
+        {
+            public void SendMessage(IMessage message)
+            {
+                message.Send();
+            }
+        }
+
+
+        class Factory
+        {
+            public static IMessage CreateMessage(string type)
+            {
+                switch (type.ToLowerInvariant())
+                {
+                    case "sms":
+                        {
+                            return new SMS();
+                        }
+
+                    case "email":
+                        {
+                            return new Email();
+                        }
+                    default:
+                        {
+                            throw new ArgumentException("Invalid Type.");
+                        }
+                }
+            }
+        }
+
+
+        class Implementation
+        {
+
+
+            public void SendMessage()
+            {
+                IMessage message1 = Factory.CreateMessage("sms");
+                IMessage message2 = Factory.CreateMessage("email");
+
+                Notify notify = new Notify();
+                notify.SendMessage(message1);
+                notify.SendMessage(message2);
+
+            }
+        }
+    }
+}
