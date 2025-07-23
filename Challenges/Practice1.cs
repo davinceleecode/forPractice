f151dd0a4714dd8b764fc2d84159134b6c006f58
davinceleecode <wiz22vincent@gmail.com>
2025-07-24 01:35:57 +0800

    Iterations, Arrays, Time Complexity

---
+++ Challenges/Practice1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace forPractice.Challenges
+{
+    public class Practice1
+    {
+        public int Solution(int N)
+        {
+
+            string binary = Convert.ToString(N, 2);
+            int countGap = 0;
+            int longestGap = 0;
+            bool isValid = false;
+
+            foreach (char item in binary)
+            {
+                if (item.ToString() == "1" && isValid == false)
+                {
+                    isValid = true;
+                    continue;
+                }
+                else if (item.ToString() == "1")
+                {
+                    longestGap = Math.Max(longestGap, countGap);
+                    countGap = 0;
+                    continue;
+                }
+                countGap++;
+            }
+            return longestGap;
+        }
+    }
+}
---
+++ Challenges/Practice3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace forPractice.Challenges
+{
+    public class Practice3
+    {
+        public int solution(int[] A)
+        {
+
+            Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+
+
+            foreach (int item in A)
+            {
+                if (keyValuePairs.ContainsKey(item))
+                    keyValuePairs[item]++;
+                else
+                    keyValuePairs.Add(item, 1);
+            }
+
+
+            var unpairedItem = keyValuePairs
+                                .Where(kv => kv.Value == 1)
+                                .Select(kv => kv.Key)
+                                .FirstOrDefault();
+
+            return unpairedItem;
+        }
+    }
+}
