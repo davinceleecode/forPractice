52edc55f8b7dda9d4bd53602b8303550f99bd183
davinceleecode <wiz22vincent@gmail.com>
2025-08-29 17:05:32 +0800

    Selection Sort

--- Challenges/SelectionSort.cs
+++ Challenges/SelectionSort.cs
@@ -8,23 +8,39 @@ namespace forPractice.Challenges
     {
         public int[] SelectionSortArr(int[] arr)
         {
+            //comparing the value
+            //4, 3, 5, 1, 2, 7, 6
 
-            for (int i = 0; i < arr.Length - 1; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
+                int currValue = arr[i];
+                int swapIndex = i;
 
-
-
-                if (arr[i] < arr[i + 1])
+                for (int x = i + 1; x < arr.Length; x++) //start from i+1
                 {
-                    int item1 = arr[i];
-                    int item2 = arr[i + 1];
+                    if (arr[swapIndex] > arr[x]) //find the smallest
+                    {
+                        swapIndex = x;
+                    }
 
-                    arr[i] = item2;
-                    arr[i + 1] = item1;
                 }
+
+
+                //swap once per outer loop
+                arr[i] = arr[swapIndex];
+                arr[swapIndex] = currValue;
+
             }
 
+
+
+
             return arr;
+
+
+
+
+
 
         }
     }
