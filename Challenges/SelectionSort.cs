69f65c6b9135b54863779e7cceeb290a9ee558b6
davinceleecode <wiz22vincent@gmail.com>
2025-08-29 12:01:01 +0800

    Binary Search

---
+++ Challenges/SelectionSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace forPractice.Challenges
+{
+    public class selectionSort
+    {
+        public int[] SelectionSortArr(int[] arr)
+        {
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+
+
+
+                if (arr[i] < arr[i + 1])
+                {
+                    int item1 = arr[i];
+                    int item2 = arr[i + 1];
+
+                    arr[i] = item2;
+                    arr[i + 1] = item1;
+                }
+            }
+
+            return arr;
+
+        }
+    }
+}
