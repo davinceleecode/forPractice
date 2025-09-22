40f9da19c990c7f2b375f80f40b31f983f10388b
davinceleecode <wiz22vincent@gmail.com>
2025-09-22 23:08:06 +0800

    Count all elements with maximum frequency

---
+++ Challenges/qeasy3005.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace forPractice.Challenges
+{
+    public class qeasy3005
+    {
+        public int MaxFrequency(int[] nums)
+        {
+            var result = nums
+     .GroupBy(x => x)
+     .Select(x => new
+     {
+         Value = x.Key,
+         Total = x.Count()
+     });
+
+            int maxFreq = result.Max(x => x.Total);
+            int total = result.Where(x => x.Total == maxFreq)
+                              .Sum(x => x.Total);
+
+            return total;
+
+        }
+    }
+}
