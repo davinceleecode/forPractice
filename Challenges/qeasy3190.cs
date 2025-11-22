077699158605ce03a64e21038e17a194c850eca6
davinceleecode <wiz22vincent@gmail.com>
2025-11-22 14:41:22 +0800

    Find Minimum Operations to make All Elements Divisible by Three

---
+++ Challenges/qeasy3190.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace forPractice.Challenges
+{
+    public class qeasy3190
+    {
+
+        public int MinimumOperations(int[] nums)
+        {
+            int result = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result += ((nums[i] + 1) % 3) == 0 ? 1 : ((nums[i] - 1) % 3) == 0 ? 1 : 0;
+            }
+            return result;
+        }
+    }
